6b8fc4d64f9d78cdb30c3a899b263fd4368424cf
rajkaur9 <rkaur12@mail.csuchico.edu>
2019-09-11 16:32:30 -0700

    Pick a number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,23 +1,27 @@
-//Author:
+//Author: Raj Kaur
 #include<iostream>
 
+using std::cin;
+using std::cout;
+using std::endl;
 int main()
 {
-  //declare variables here
 
-  std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
+        int first = 0;
+        int second = 0;
+        int biggest = 0;
 
-  std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+  std::cout<<"Please enter a whole number:";
+  std::cin>>first;
 
-  //determine if first or second is bigger and store that in a third variable
+  std::cout<<"Please enter another whole number:";
+  std::cin>>second;
 
 
-  std::cout<<"Of those two numbers, the biggest is: ";
-  //print out the variable representing the biggest number
+  std::cout<<"Of those two numbers, the biggest is: "<<biggest<<endl;
+  std::cin>>biggest;
 
-  std::cout<<std::endl<<"Thank you for playing.\n";
+  std::cout<<std::endl<<"Thank you for playing.";
   
   return 0;
 }
